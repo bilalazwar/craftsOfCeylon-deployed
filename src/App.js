cbf30e6447b0c1453a1fd6a5381360d173e51aa7
Bilal Azwar <bilalazwar@gmail.com>
2024-02-23 15:21:40 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-// import logo from './logo.svg';
-// import './App.css';
+// React_Router-Dom is a Library within react that is used to implement client side routing in react application
+
+import './App.css';   // app.css is a global styling.. when i Removed my Home page didn't display
+import React from "react";
+import { BrowserRouter, Route, Routes} from 'react-router-dom'    // needed to install before writing this code ["npm install react-router-dom"]
+import Home from './page/Home';
+
 
 function App() {
   return (
-   <div>
-    <h1>HEllo World</h1>
-    <p>Welocome mr jdhd dhfnd</p>
-    <p>made changes brother</p>
-    <p>Hello Sri lanka ..................................</p>
-   </div>
+    <div className="App">
+      <BrowserRouter>
+      <Routes>      {/* like a container to have all the child route, can omit this if have only 1 route*/}
+
+          <Route index element={<Home/>}/>
+
+          {/* <Route path="*" element={<GIve NOT FOUND/>}/> */}
+          
+
+      </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
