b2746e5bf837a3fb3b6263f1dc346cebe2061cf1
Bilal Azwar <bilalazwar@gmail.com>
2024-02-23 15:31:30 +0530

    changes-3

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@
 
 import './App.css';   // app.css is a global styling.. when i Removed my Home page didn't display
 import React from "react";
-import { BrowserRouter, Route, Routes} from 'react-router-dom'    // needed to install before writing this code ["npm install react-router-dom"]
+import { BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'    // needed to install before writing this code ["npm install react-router-dom"]
 import Home from './page/Home';
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
       <Routes>      {/* like a container to have all the child route, can omit this if have only 1 route*/}
 
           <Route index element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
           <Route path='*' element={<Home/>}/>
 
           {/* <Route path="*" element={<GIve NOT FOUND/>}/> */}
           
 
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
